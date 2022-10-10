08230c1bcc6a415c8d935431ecbd8e45c10a66d4
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-10-10 22:25:16 +0300

    Initial Commit

---
+++ 008_CardsStack/_08_CardsStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  _08_CardsStackApp.swift
+//  008_CardsStack
+//
+//  Created by nikita on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct _08_CardsStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
